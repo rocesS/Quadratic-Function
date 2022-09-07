4b595ce08b69a904ba3bf4f96b4df19541a80800
Kamil <kamil@klemczak.pl>
2022-09-07 23:49:53 +0200

    add new method "countDelta"

---
+++ src/pl/klemi/Main.java
@@ -0,0 +1,25 @@
+package pl.klemi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int c;
+        int delta;
+
+        /*switch (delta)
+        case 1:
+        case 2:
+        default:
+        */
+
+
+    }
+
+    public static int countDelta(int a, int b, int c) {
+        return b*b+4*a*c;
+    }
+
+}
